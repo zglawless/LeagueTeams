d4d258104e7c043088207b16794111ad9510ec4d
zglawless <112585195+zglawless@users.noreply.github.com>
2022-10-11 20:42:51 -0500

    First Commit

---
+++ src/main/java/model/Players.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="players")
+public class Players {
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	private int id;
+	@Column(name="NAME")
+	private String name;
+	@Column(name="NUMBER") // stores the players jersey number
+	private int number;
+	@Column(name="STARTDATE") // stores the date the player started playing on the team
+	private LocalDate startDate;
+	
+	public Players() {
+
+	}
+
+	public Players(int id, String name, int number, LocalDate startDate) {
+		this.id = id;
+		this.name = name;
+		this.number = number;
+		this.startDate = startDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Teams [id=" + id + ", name=" + name + ", number=" + number + ", startDate=" + startDate + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/model/Teams.java
@@ -0,0 +1,52 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="teams")
+public class Teams {
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	private int id;
+	@Column(name="TEAMNAME")
+	private String teamName;
+	
+	public Teams() {
+
+	}
+
+	public Teams(int id, String teamName) {
+		super();
+		this.id = id;
+		this.teamName = teamName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	@Override
+	public String toString() {
+		return "Teams [id=" + id + ", teamName=" + teamName + "]";
+	}
+	
+	
+	
+}
